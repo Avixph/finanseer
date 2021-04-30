45a62815e3ef27ff9eddc36896c35e593436d054
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-04-30 17:58:24 -0400

    pulling latest

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,14 +13,14 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.get("/api/hello", (req, res) => {
-//   res.send({ express: "Hello From Express" });
-// });
-// app.post("/api/world", (req, res) => {
-//   console.log(req.body);
-//   res.send(
-//     `I received your POST request. This is what you sent me: ${req.body.post}`
-//   );
-// });
+app.get("/api/hello", (req, res) => {
+  res.send({ express: "Hello From Express" });
+});
+app.post("/api/world", (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`
+  );
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
